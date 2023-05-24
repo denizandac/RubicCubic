92153625141812e000b1cfec8ae82030b883bf46
Deniz <denizandac33@gmail.com>
2023-05-24 14:43:57 +0300

    Adjusted Mouse Drag Mechanism
    - Player and camera jitters are greately reduced.
    - Clamp added.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,39 +5,43 @@ using DG.Tweening;
 
 public class PlayerController : MonoBehaviour
 {
-    public static PlayerController _instance;
+    public static PlayerController instance;
     // Start is called before the first frame update
 
     [Header("Movement")]
-    [SerializeField] private float _horizontalSpeed;
+    [SerializeField] private float _horizontalSpeed = 25f;
     [SerializeField] private float _frontalSpeed = 5f;
-    [SerializeField] private float _jumpHeight = 5f;
     [SerializeField] private float _gravityForce = 9.81f;
     [SerializeField] private float _roadWidth = 10f;
+    [SerializeField] private float _playerWidth = 1f;
+    //[SerializeField] private float _jumpHeight = 10f;
 
     [Header("References")]
     [SerializeField] private Rigidbody _rb;
     [SerializeField] private Animator _anim;
 
     [Header("Ground Check")]
-    [SerializeField] private bool _isGrounded;
-    [SerializeField] private bool _willJump;
-    [SerializeField] private float _groundCheckRadius = 0.5f;
+    [SerializeField] private float _groundCheckRadius = 1.2f;
     [SerializeField] private LayerMask _groundLayer;
+    [SerializeField] private bool _isGrounded;
+    //[SerializeField] private bool _willJump;
 
     [Header("Mouse Input")]
-    [SerializeField] private Vector2 _startTouchPosition;
-    [SerializeField] private Vector2 _currentTouchPosition;
-    [SerializeField] private Vector2 _touchDelta;
-    [SerializeField] private float _touchThreshold = 0.7f;
-    [SerializeField] private float _touchSensitivity = 75f;
+    [SerializeField] private Vector3 _initialPosition;
+    [SerializeField] private bool _isDragging = false;
+    [SerializeField] private float _deltaX;
+    [SerializeField] private float _dragStartX;
+    [SerializeField] private float _targetX;
+    [SerializeField] private float _LerpSpeed = 10f;
+    //[SerializeField] private float _touchSensitivity = 100f;
+    //[SerializeField] private float _touchThreshold = 0.7f;
 
 
     void Awake()
     {
-        if(_instance == null)
+        if(instance == null)
         {
-            _instance = this;
+            instance = this;
         }
         else
         {
@@ -52,28 +56,60 @@ public class PlayerController : MonoBehaviour
     }
 
     void Update(){
+        _initialPosition.y = transform.position.y;
+        _initialPosition.z = transform.position.z;
         if(Physics.CheckSphere(new Vector3(0f,transform.position.y, 0f), _groundCheckRadius, _groundLayer)){
             _isGrounded = true;
         }
         else{
             _isGrounded = false;
         }
-        if(Input.GetKeyDown(KeyCode.Space) && _isGrounded){
-            _willJump = true;
-        }
         if(Input.GetMouseButtonDown(0)){
-            _startTouchPosition = Input.mousePosition;
+            _initialPosition.x = transform.position.x;
+            _isDragging = true;
+            _dragStartX = Input.mousePosition.x;
+        }
+        else if(Input.GetMouseButtonUp(0)){
+            _isDragging = false;
         }
         if(Input.GetMouseButton(0)){
-            _currentTouchPosition = Input.mousePosition;
-            _touchDelta = _currentTouchPosition-_startTouchPosition;
-            _horizontalSpeed = (_touchDelta.x * _roadWidth * _touchSensitivity)/ Screen.width;
-            _startTouchPosition = _currentTouchPosition;
+            _isDragging = true;
         }
+        if (_isDragging){   
+            float _deltaX = (Input.mousePosition.x - _dragStartX) * _horizontalSpeed / Screen.width;
+            _targetX = _initialPosition.x + _deltaX;
+            _targetX = Mathf.Clamp(_targetX, -_roadWidth/2 + _playerWidth/2, _roadWidth/2 - _playerWidth/2);
+        }
+        // if(Input.GetKeyDown(KeyCode.Space) && _isGrounded){
+        //     _willJump = true;
+        // }
     }
     // Update is called once per frame
     void FixedUpdate()
     {
+
+        // Mouse Input for Mobile
+        if (_isDragging)
+        {
+            Vector3 targetPosition = new Vector3(_targetX, _initialPosition.y, _initialPosition.z);
+            transform.position = Vector3.Lerp(transform.position, targetPosition, Time.fixedDeltaTime*_LerpSpeed);
+        }
+
+        transform.Translate(Vector3.forward * _frontalSpeed * Time.deltaTime);
+
+        if(!_isGrounded){
+            transform.Translate(Vector3.down * _gravityForce * Time.deltaTime);
+        }
+        #region Jumping       
+        // Jumping (if needed)
+        // if(_willJump && _isGrounded)
+        // {
+        //     transform.Translate(Vector3.up * _jumpHeight * Time.deltaTime);
+        //     _isGrounded = false;
+        //     _willJump = false;
+        // }
+        #endregion
+        #region Keyboard Input for Mobile
         // Keyboard Input for PC
         // if(Input.GetKey(KeyCode.A))
         // {
@@ -83,24 +119,6 @@ public class PlayerController : MonoBehaviour
         // {
         //     transform.Translate(Vector3.right * _horizontalSpeed * Time.deltaTime);
         // }
-
-        // Mouse Input for Mobile
-
-        if(Input.GetMouseButton(0)){
-            if(_horizontalSpeed > _touchThreshold || _horizontalSpeed < -_touchThreshold){
-                transform.Translate(Vector3.right * _horizontalSpeed * Time.deltaTime);
-            }
-        }
-
-        transform.Translate(Vector3.forward * _frontalSpeed * Time.deltaTime);
-        if(_willJump && _isGrounded)
-        {
-            transform.DOJump(transform.position, _jumpHeight, 1, 1f).SetEase(Ease.InOutSine);
-            _isGrounded = false;
-            _willJump = false;
-        }
-        if(!_isGrounded){
-            transform.Translate(Vector3.down * _gravityForce * Time.deltaTime);
-        }        
+        #endregion
     }   
 }
