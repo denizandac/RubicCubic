8111de91c95712c062eebedf4265cc7aae04a8b5
Deniz <denizandac33@gmail.com>
2023-05-24 14:46:28 +0300

    Added new methods
    New methods are added to CubeHandler and GameManager scripts to keep track of collection and save the datas.

--- Assets/Scripts/CubeHandler.cs
+++ Assets/Scripts/CubeHandler.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class NewBehaviourScript : MonoBehaviour
+public class CubeHandler : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] private Rigidbody _rb;
+    [SerializeField] private bool _isTaken = false;
+
+
+
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnTriggerEnter(Collider other){
+        if(other.CompareTag("Player") && !_isTaken){
+            _isTaken = true;
+            GameManager.instance.collectCube();
+            this.gameObject.SetActive(false);
+        }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    public static GameManager _instance;
+    public static GameManager instance;
+
+    [SerializeField] private int _score;
+    [SerializeField] private int _cubeCount;
+    [SerializeField] private int _cubeCountMax;
+    [SerializeField] private int _lastCubeCount;
+    [SerializeField] private Vector2 _lastPosition;
+    [SerializeField] private Vector3 _lastRotation;
+    //private GameManagerState _gameManagerState;
+
     void Awake()
     {
-        if(_instance == null)
+        if(instance == null)
         {
-            _instance = this;
+            instance = this;
         }
         else
         {
@@ -18,14 +27,37 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    void Start(){
+        _score = 0;
+        _cubeCount = 0;
+        _cubeCountMax = 5;
+        _lastCubeCount = 0;
+        _lastPosition = PlayerController.instance.transform.position;
+        _lastRotation = PlayerController.instance.transform.rotation.eulerAngles;
+        //_gameManagerState = GameManagerState.Start;
+    }
     // Update is called once per frame
     void Update()
     {
         
     }
+
+    public void collectCube(){
+        if(_cubeCount < _cubeCountMax){
+            _cubeCount++;
+        }
+        _score+=100;
+        //extract from pool
+    }
+
+    private void saveFromCheckpoint(){
+        _lastCubeCount = _cubeCount;
+        _lastPosition = PlayerController.instance.transform.position;
+        _lastRotation = PlayerController.instance.transform.rotation.eulerAngles;
+    }
+
     enum GameManagerState { 
         Start,
-        Playing,
         GameOver
     }
 }
